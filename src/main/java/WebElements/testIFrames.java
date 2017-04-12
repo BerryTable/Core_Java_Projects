670b014625ccbed9b1fb62fe8faacf2ac31208dd
MG <mamthag@gmail.com>
2017-04-12 09:26:01 +0530

    Added A test Login testcase

---
+++ src/main/java/WebElements/testIFrames.java
@@ -0,0 +1,18 @@
+package WebElements;
+
+/**
+ * Created by MamathaGangappa on 4/11/17.
+ */
+/*public class testIFrames {
+    WebDriver driver = new FirefoxDriver();
+		driver.get("http://toolsqa.wpengine.com/iframe-practice-page/");
+
+    //By executing a java script
+    JavascriptExecutor exe = (JavascriptExecutor) driver;
+    Integer numberOfFrames = Integer.parseInt(exe.executeScript("return window.length").toString());
+		System.out.println("Number of iframes on the page are " + numberOfFrames);
+
+    //By finding all the web elements using iframe tag
+    List<WebElement> iframeElements = driver.findElements(By.tagName("iframe"));
+		System.out.println("The total number of iframes are " + iframeElements.size());
+} */
